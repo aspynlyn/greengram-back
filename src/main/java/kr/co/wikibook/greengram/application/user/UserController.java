e5d2e84ee4b69572486f207ff530a059c23b30f5
aspynlyn <lyn.aspyn@gmail>
2025-08-06 17:21:21 +0900

    그린그램 vaildation

--- src/main/java/kr/co/wikibook/greengram/application/user/UserController.java
+++ src/main/java/kr/co/wikibook/greengram/application/user/UserController.java
@@ -1,11 +1,15 @@
 package kr.co.wikibook.greengram.application.user;
 
+import jakarta.validation.Valid;
+import kr.co.wikibook.greengram.application.user.model.UserSignUpReq;
 import kr.co.wikibook.greengram.config.model.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Slf4j
@@ -15,7 +19,10 @@ public class UserController {
   private final UserService userService;
 
   @PostMapping("/sign-up")
-  public ResultResponse<?> signUp(){
+  public ResultResponse<?> signUp(@Valid @RequestPart UserSignUpReq req, @RequestPart(required = false) MultipartFile pic){
+    log.info("req: {}", req);
+    log.info("pic: {}", pic != null ? pic.getOriginalFilename() : pic);
     return new ResultResponse<Integer>("",1 );
+
   }
 }
